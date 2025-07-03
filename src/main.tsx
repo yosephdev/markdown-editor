7c1f81ccd7e7df1f1586804ea76d4a9016caa3a1
Yoseph <yosephbet@gmail.com>
2025-07-03 07:58:23 +0200

    refactor: Wrap App in React.StrictMode for development checks

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
